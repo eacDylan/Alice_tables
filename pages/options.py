c1c06a9416ff9555b459cf644a6bf5f838838b69
eacDylan <167919974+eacDylan@users.noreply.github.com>
2025-08-30 07:55:41 +0200

    Solution to issue #2
    The user can download the result in a csv file.

--- pages/options.py
+++ pages/options.py
@@ -4,13 +4,13 @@ import functions
 
 st.title("Options sur le DataFrame")
 
-# Bouton pour réinitialiser
+# --- The user can reset the dataframe ---
 if st.button("Réinitialiser le DataFrame"):
     st.session_state.df = functions.generate_dataframe()
     st.session_state.df.to_csv(st.session_state.file_path, index=False, sep=";")
     st.success("Le DataFrame a été réinitialisé ✅")
 
-# Upload d’un fichier CSV
+# --- The user can upload the dataframe from a CSV file ---
 uploaded_file = st.file_uploader("Charger un fichier CSV", type="csv")
 
 if uploaded_file is not None:
@@ -18,7 +18,22 @@ if uploaded_file is not None:
     st.session_state.df.to_csv(st.session_state.file_path, index=False, sep=";")
     st.success("Nouveau DataFrame chargé !")
 
-# Ajouter la possibilité d'exporter un dataframe au format csv
+# --- The user can download the dataframe to a CSV file ---
+# dataframe => CSV conversion
+csv = st.session_state.df.to_csv(index=False, sep=";")
+
+file_name = st.text_input(
+    label="Nom du fichier à exporter :",
+      value="Tables_Alice",
+      key="file_name"
+)
+
+st.download_button(
+    label="📥 Télécharger les données (CSV)",
+    data=csv,
+    file_name=file_name,
+    mime="text/csv"
+)
 
 # Afficher le DataFrame actuel
 st.write("DataFrame actuel :")
